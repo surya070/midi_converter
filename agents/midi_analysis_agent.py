61aadecb5860d35866636ce81b30a388fd423887
Surya Maheswaran <94157626+surya070@users.noreply.github.com>
2025-12-26 15:05:13 +0530

    Add files via upload

--- agents/midi_analysis_agent.py
+++ agents/midi_analysis_agent.py
@@ -5,13 +5,16 @@ class MIDIAnalysisAgent:
         midi = pretty_midi.PrettyMIDI(midi_path)
         notes = []
 
+        tempo = midi.estimate_tempo()
+        seconds_per_quarter = 60.0 / tempo
+
         for inst in midi.instruments:
             for n in inst.notes:
                 notes.append({
                     "pitch": n.pitch,
-                    "start": n.start,
-                    "duration": n.end - n.start
+                    "offset": n.start / seconds_per_quarter,
+                    "duration": (n.end - n.start) / seconds_per_quarter
                 })
 
-        notes.sort(key=lambda x: (x["start"], -x["pitch"]))
-        return notes, midi.estimate_tempo()
+        notes.sort(key=lambda x: (x["offset"], -x["pitch"]))
+        return notes, tempo
